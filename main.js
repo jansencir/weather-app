2059719f60b3ca539a3714a9f9bf8e6dd876d565
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-01-26 10:42:36 -1000

    Write pseudocode for project

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+/**Pseudocode
+ * Grab user input
+ *  Check if the city exists
+ *    If city exists, grab info
+ *    If city doesn't, notify user
+ * Fetch city from VC API
+ *  Turn into a JS object
+ *  Start grabbing info you want
+ *  Display the info you want in the specific area
+ * Color changing weather
+ *  Take each general weather condition
+ *  Set color depending on that weather condition
+ */
